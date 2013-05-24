e6ba921f4514cb8b1210ff0733137cc712cfb3be
Michael Lai <themichaellai@gmail.com>
2013-05-24 15:37:38 +0800

    ability to edit track name with whatever

--- tag_check.rb
+++ tag_check.rb
@@ -26,7 +26,7 @@ end
 
 def get_top_tracks(artist)
   artist_param = artist.gsub(/ /, '+')
-  method = "?method=artist.gettoptracks&artist=#{artist_param}&api_key=#{APIKEY}&format=json&limit=200"
+  method = "?method=artist.gettoptracks&artist=#{artist_param}&api_key=#{APIKEY}&format=json&limit=500"
   data = JSON.parse open(APIROOT + method).read
   data['toptracks']['track'].map do |track|
     {
@@ -54,16 +54,21 @@ if __FILE__ == $0
       end
       similar.sort_by!{|s| levenschtein(track_title, s[:name]) }
       if similar.length > 0
-        if similar[0][:name].eql? info.tag.title
+        if similar[0][:name].eql? info.tag.title and similar.length == 1
           puts "Already matches top!".green
         else
           puts "Similar: "
           similar.each_with_index do |sim, i|
             puts "#{i+1} #{sim[:name]} (#{levenschtein(track_title, sim[:name])}) (#{sim[:listeners]})"
           end
-          print 'Replace with ? (#/[n]) '.red
+          print 'Replace with ? (#/[n]/e) '.red
           choice = gets.chomp
-          if choice.eql? 'y' or (1..similar.length).include? choice.to_i
+          if choice.eql? 'e'
+            puts 'Replace with? '
+            new_name = gets.chomp
+            puts "Changing to: #{new_name}".blue
+            info.tag.title = new_name
+          elsif choice.eql? 'y' or (1..similar.length).include? choice.to_i
             puts "Changing to: #{similar[choice.to_i - 1][:name]}".blue
             info.tag.title = similar[choice.to_i - 1][:name]
           end
